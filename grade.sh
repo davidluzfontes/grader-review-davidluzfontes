88f8e21f7999b642e154e843b65c98337d50ac07
David Fontes <dsaldanhadaluzfontes@ucsd.edu>
2024-02-15 13:50:22 -0800

    q

--- grade.sh
+++ grade.sh
@@ -1,4 +1,5 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
+RUNPATH='.;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -8,7 +9,7 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
-if [ -f "student-submission/ListExamples.java"]; then
+if [ -f "student-submission/ListExamples.java" ]; then
     echo "File found"
 else
     echo "File not found"
@@ -16,12 +17,35 @@ else
 fi
 
 cp student-submission/ListExamples.java grading-area/
-cp TestListExamples.java grading-area
-cp -r lib/ grading-area/
+cp TestListExamples.java grading-area/
+cp -r lib grading-area
 
 cd grading-area
-javac -cp ("$CPATH" *.java )
+javac -cp $CPATH *.java 
 
+if [ $? -ne 0 ]; then
+    echo "Compilation error!"
+    GRADE=0
+    echo "Grade:" $GRADE
+    exit 1
+fi
+
+echo "Program compiled successfully"
+
+java -cp $RUNPATH TestListExamples | tail -n 2 | head -n 1 > test-result.txt 
+
+if ($(cut -d' ' -f 1 test-result.txt) == "OK" ); then
+    GRADE=100
+    echo "Grade:" $GRADE
+    exit 0
+fi
+echo errors
+cut -d' ' -f 6 test-result.txt 
+echo total
+cut -d' ' -f 3 test-result.txt | cut -d',' -f 1 
+
+echo $FAILS
+exit 0
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
