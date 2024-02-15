41ca9acc66f3e851dcd5295f03ba1aa9171524e5
David Fontes <dsaldanhadaluzfontes@ucsd.edu>
2024-02-15 12:17:06 -0800

    do

--- grade.sh
+++ grade.sh
@@ -8,6 +8,13 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [ -f "student-submission/ListExamples.java"]; then
+    echo "File found"
+else
+    echo "File not found"
+    exit(1)
+fi
+
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
